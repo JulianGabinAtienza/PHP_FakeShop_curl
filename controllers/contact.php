be58ea17c2feca6f004d33388723e6a616095d53
JulianGabinAtienza <j.atienza@ecole-ipssi.net>
2024-01-29 16:35:13 +0100

    :bug:

--- controllers/contact.php
+++ controllers/contact.php
@@ -4,50 +4,50 @@
     require_once '../config/php_mailer.php';
     require_once '../vendor/autoload.php';
 
-        use PHPMailer\PHPMailer\PHPMailer;
-        use PHPMailer\PHPMailer\Exception;
-        use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP;
 
-        // Récupération des données rentrées par l'utilisateur du formulaire de contact 
-        if (($_SERVER['REQUEST_METHOD'] === 'POST')) {
-            $email = $_POST['email'];
-            $subject = htmlspecialchars($_POST['subject']);
-            $body = htmlspecialchars($_POST['body']);
+    // Récupération des données rentrées par l'utilisateur du formulaire de contact 
+    if (($_SERVER['REQUEST_METHOD'] === 'POST')) {
+        $email = $_POST['email'];
+        $subject = htmlspecialchars($_POST['subject']);
+        $body = htmlspecialchars($_POST['body']);
 
-            // On vérifie que l'email soit au bon format
-            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // On vérifie que l'email soit au bon format
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
 
-                    // On instancie la classe PHP Mailer cad on crée un objet $mail
-                $mail = new PHPMailer(true);
+            // On instancie la classe PHP Mailer cad on crée un objet $mail
+            $mail = new PHPMailer(true);
 
-                try {
-                    //Server settings / On configure le serveur SMTP
-                    $mail->isSMTP();                                            
-                    $mail->Host       = SMTP_HOST;                     
-                    $mail->SMTPAuth   = true;                                   
-                    $mail->Username   = SMTP_USERNAME;                     
-                    $mail->Password   = SMTP_PASSWORD;                               
-                    $mail->SMTPSecure = SMTP_ENCRYPTION;            
-                    $mail->Port       = SMTP_PORT;                              
+            try {
+                //Server settings / On configure le serveur SMTP
+                $mail->isSMTP();                                            
+                $mail->Host       = SMTP_HOST;                     
+                $mail->SMTPAuth   = true;                                   
+                $mail->Username   = SMTP_USERNAME;                     
+                $mail->Password   = SMTP_PASSWORD;                               
+                $mail->SMTPSecure = SMTP_ENCRYPTION;            
+                $mail->Port       = SMTP_PORT;                              
 
-                    //Recipients / On précise les récipients pour le mail 
-                    $mail->setFrom($email);
-                    $mail->addAddress($mail->Username, 'Romain'); 
-                    $mail->Subject = $subject;
-                    $mail->Body  = $body;   //Add a recipient
+                //Recipients / On précise les récipients pour le mail 
+                $mail->setFrom($email);
+                $mail->addAddress($mail->Username, 'Julian'); 
+                $mail->Subject = $subject;
+                $mail->Body  = $body;   //Add a recipient
 
-                    // On envoie le mail 
-                    $mail->send();
-                    echo 'Message has been sent';
+                // On envoie le mail 
+                $mail->send();
+                echo 'Message has been sent';
 
-                } catch (Exception $e) {
-                    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-                }
-            } else {
-                $error = "Assurez vous que le mail soit au bon format";
+            } catch (Exception $e) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
             }
         } else {
-            $error = "Veuillez remplir tous les champs";
+            $error = "Assurez vous que le mail soit au bon format";
         }
+    } else {
+        $error = "Veuillez remplir tous les champs";
+    }
 
-    ?>
+?>
