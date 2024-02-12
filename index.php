6334b186dcc29cea68bc2e4f52ec1dc169f7681a
JulianGabinAtienza <j.atienza@ecole-ipssi.net>
2024-02-12 11:54:38 +0100

    :memo: Update

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php 
+
+session_start();
+
+?>
 <link rel="stylesheet" href="./views/style/style.css">
 <script src="./views/scripts/app.js" defer></script>
     <nav>
@@ -8,9 +13,9 @@
 
             <?php if (isset($_SESSION['user']) && $_SESSION['user']['logged']) :  ?> 
 
-                <li><a href="/views/products.view.php">Products</a></li>
-                <li><a href="/views/profile.view.php">Profile</a></li>
-                <li><a href="/views/logout.php">Logout</a></li>
+                <li><a href="views/products.view.php">Products</a></li>
+                <li><a href="views/profile.view.php">Profile</a></li>
+                <li><a href="views/logout.php">Logout</a></li>
             
             <?php else : ?>
                 <li><a href="/views/signup.view.php">Signup</a></li>
@@ -25,6 +30,5 @@
         <h1>Bienvenue sur mon app en PHP !</h1>
         <img class="home-img" class="home-cocktail" src="./assets/images/cocktail.png" alt="Cocktail PHP">
     </div>
-
 
 <?php include "partials/footer.php"; ?>
