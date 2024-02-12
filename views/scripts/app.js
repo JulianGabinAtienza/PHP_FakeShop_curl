9e74120b231f69402edf809cbebd98750cf7ae3f
JulianGabinAtienza <j.atienza@ecole-ipssi.net>
2024-02-12 16:55:16 +0100

    :memo: JS Update

--- views/scripts/app.js
+++ views/scripts/app.js
@@ -2,6 +2,19 @@ const burger = document.querySelector('.burger-btn');
 const menu = document.querySelector('.menu');
 
 burger.addEventListener('click', () => {
-    menu.style.opacity = menu.style.opacity === '1' ? '0' : '1';
-    menu.style.left = menu.style.opacity === '1' ? '20rem' : '-20rem';
-});
+    // Avec toggle on fait la meme chose que plus bas
+    menu.classList.toggle('open');
+
+    // Un a un if pour ajouter la classe open à notremenu si il ne l'a pas déjà
+    // Sinon on lui enlève cette classe
+    // if (!menu.classList.contains('open')) {
+    //     menu.classList.add('open');
+    // } else {
+    //     menu.classList.remove('open');
+    // }
+
+    // Pour ajouter une animation lors du click sur mon burger
+    burger.classList.toggle('animate__animated');
+    burger.classList.toggle('animate__swing');
+
+})
