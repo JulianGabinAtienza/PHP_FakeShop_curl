a61856ea81ecbfeebb4854ac811a167781c33a2e
JulianGabinAtienza <j.atienza@ecole-ipssi.net>
2024-02-28 10:19:47 +0100

    todo, et name random giver

--- exercices/randomNameGiver/script.js
+++ exercices/randomNameGiver/script.js
@@ -1,29 +1,4 @@
-const allName = [
-    'ADAM Carter',
-    'ANNASSAMY Marc',
-    'ATIENZA Julian',
-    'BEN MAHFOUDH Chaïma',
-    'BLOCTEUR Arthur',
-    'BOILEAU Maxim',
-    'DEVOUCHE Zacharie',
-    'DUNCA Denis',
-    'ESSONGHE Alex',
-    'FDHIL Elyea',
-    'GHEZALI Camelia',
-    'HASSANI Ayoub',
-    'KABA Ibrahima',
-    'KHIF Sara',
-    'LALANNE Samuel',
-    'MABICKASSA BOUSSOUGOU Serge-Daryl',
-    'MARTIN Gabriel',
-    'OTABELA Juan Miguel',
-    'RICHER Benjamin',
-    'SOGLO Helsy Aubierge',
-    'TARCHOUN Rayan',
-    'THABTHIM Kevin',
-    'TROUVE Rhys',
-    'YAKOU Franckamour',
-];
+import allName from './allName.js'
 
 const randomNameGiver = () => {
     const randomIndex = Math.floor(Math.random() * allName.length);
